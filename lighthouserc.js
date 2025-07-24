bf0f8baead2a3d3a5e305e3928bdfc4d925bc36e
FranceHuon <francehuon@gmail.com>
2025-07-24 17:26:08 +0200

    Update ci.yml again and again and again and again and again and again and again and again

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,6 +7,7 @@ export default {
       startServerCommand: 'docker compose up --build -d',
       startServerReadyPattern: 'Container gazette-frontend-1.*Started',
       startServerReadyTimeout: 60000,
+      numberOfRuns: 1,
     },
     upload: {
       target: 'temporary-public-storage',
