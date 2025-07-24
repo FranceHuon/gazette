ab2f12ae0b19119feb769dc615e6a1a188d61557
FranceHuon <francehuon@gmail.com>
2025-07-24 17:31:37 +0200

    Update ci.yml again and again and again and again and again and again and again and again

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,6 @@
-import process from 'node:process'
-
 export default {
   ci: {
     collect: {
-      url: [`http://localhost:${process.env.FRONTEND_PORT || 3002}/`],
       startServerCommand: 'docker compose up --build -d',
       startServerReadyPattern: 'Container gazette-frontend-1.*Started',
       startServerReadyTimeout: 60000,
