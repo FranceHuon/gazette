58c60a32cf303b05c60603978bcda4e5b8ac07d6
elinelgp <elinelgp@gmail.com>
2025-07-26 23:36:34 +0200

    feat: add swagger for api doc

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
 import { Logger } from 'nestjs-pino'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,7 +15,15 @@ async function bootstrap() {
       allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With, Origin',
       exposedHeaders: 'Set-Cookie',
     },
-  })
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Gazette API')
+    .setDescription('API pour l\'application Gazette')
+    .setVersion('1.0')
+    .addTag('gazette')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
