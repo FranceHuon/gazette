5e00e45735f24a58be128b2fe7b33f84b541a9e3
FranceHuon <francehuon@gmail.com>
2025-08-12 11:59:15 +0200

    update landing page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,23 +1,40 @@
 'use client'
 
-import { Flex, Heading } from '@chakra-ui/react'
+import { Flex, Heading, Image, Link, Text } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 
 export default function LandingPage() {
   const { t } = useTranslation()
 
   return (
-    <Flex justifyContent="center" alignItems="center" height="100vh" width="100vw" bgColor="chaletGreen">
-      <Heading
-        size={{ base: 'lg', md: 'xl' }}
-        fontSize={{ base: 'lg', md: 'xl' }}
-        lineHeight="1.2"
-        color="white"
-        textAlign="center"
-        fontFamily="Staatliches"
-      >
-        {t('common.appDescription')}
-      </Heading>
+    <Flex height="100vh" width="100vw" bgColor="chaletGreen">
+      <Flex direction="column" alignItems="flex-start" justifyContent="center" gap="16px" width="40%" padding="24px">
+
+        <Heading
+          size={{ base: 'lg', md: 'xl' }}
+          fontSize={{ base: '40px', md: '74px' }}
+          lineHeight="1.2"
+          color="white"
+          textAlign="left"
+          fontFamily="Staatliches"
+        >
+          {t('common.appTitle')}
+        </Heading>
+
+        <Text fontSize="20px" lineHeight="1.8" color="white">Gazette est un agrégateur de flux RSS moderne qui centralise vos sources d'actualités préférées. L'application vous permet de vous abonner à des médias, de consulter leurs articles dans une interface épurée, et de créer votre propre bibliothèque de contenus favoris. Simple et entièrement gratuit.</Text>
+        <Link href="/signup" fontSize="20px" lineHeight="1.8" color="white" fontWeight="bold">{t('auth.signup')}</Link>
+        <Link href="/login" fontSize="20px" lineHeight="1.8" color="white" fontWeight="bold">{t('auth.login')}</Link>
+      </Flex>
+      <Flex width="60%" justifyContent="center" alignItems="center">
+        <Image
+          src="/explore.png"
+          alt="explore"
+          maxWidth={{ base: '200px', md: '600px' }}
+          height="auto"
+          objectFit="contain"
+        />
+      </Flex>
+
     </Flex>
   )
 }
